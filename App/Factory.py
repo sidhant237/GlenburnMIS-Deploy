88e69323e4c748aa8527359f77b9dcd31ac59b50
joshyjoy <joshy.joy@cognicor.com>
2020-08-13 21:58:50 +0530

    done

--- App/Factory.py
+++ App/Factory.py
@@ -21,38 +21,38 @@ def displayfactory():
       rv = []
       ##TEA MADE
       # [TM TODAY]
-      val = "TMENTRY.TM_VAL "
-      tab = "TMENTRY"
-      cur.execute(f'''select {val} from {tab} where TM_DATE = {d1} ''')
+      val = "TMEntry.TM_Val "
+      tab = "TMEntry"
+      cur.execute(f'''select {val} from {tab} where TM_Date = {d1} ''')
       rv.append(cur.fetchall()[0][0])
 
       # [TM TODATE]
-      val1 = "sum(TMENTRY.TM_VAL)"
-      tab1 = "TMENTRY"
-      cur.execute(f'''select {val1} from {tab1} where TM_DATE >= {d0} AND TM_DATE <= {d1} ''')
+      val1 = "sum(TMEntry.TM_Val)"
+      tab1 = "TMEntry"
+      cur.execute(f'''select {val1} from {tab1} where TM_Date >= {d0} AND TM_Date <= {d1} ''')
       rv.append(cur.fetchall()[0][0])
 
       # [TM TODATE LAST YEAR]
-      val2 = "sum(TMENTRY.TM_VAL)"
-      tab2 = "TMENTRY"
-      cur.execute(f'''select {val2} from {tab2} where TM_DATE >= {d00} AND TM_DATE <= {d11} ''')
+      val2 = "sum(TMEntry.TM_Val)"
+      tab2 = "TMEntry"
+      cur.execute(f'''select {val2} from {tab2} where TM_Date >= {d00} AND TM_Date <= {d11} ''')
       rv.append(cur.fetchall()[0][0])
 
       # [RECOVERY % TODAY
-      val3 = " ROUND(SUM(FIELDENTRY.GL_VAL)/SUM(TMENTRY.TM_VAL),4) * 100 "
-      tab3 = "TMENTRY , FIELDENTRY"
-      joi3 = "(TMENTRY.TM_DATE = FIELDENTRY.DATE) and (TMENTRY.TM_DATE="
+      val3 = " ROUND(SUM(FieldEntry.GL_Val)/SUM(TMEntry.TM_Val),4) * 100 "
+      tab3 = "TMEntry , FieldEntry"
+      joi3 = "(TMEntry.TM_Date = FieldEntry.Date) and (TMEntry.TM_Date="
       cur.execute(f'''select {val3} from {tab3} where {joi3}{d1})''')
       rv.append(cur.fetchall()[0][0])
 
       # [RECOVERY % TO DATE
-      val4 = " ROUND(SUM(FIELDENTRY.GL_VAL)/SUM(TMENTRY.TM_VAL),4) * 100 "
-      tab4 = 'TMENTRY , FIELDENTRY'
-      joi4 = "(TMENTRY.TM_DATE = FIELDENTRY.DATE) and (TMENTRY.TM_DATE>="
-      cur.execute(f'''select {val4} from {tab4} where {joi4}{d0}) and (TMENTRY.TM_DATE<={d1})''')
+      val4 = " ROUND(SUM(FieldEntry.GL_Val)/SUM(TMEntry.TM_Val),4) * 100 "
+      tab4 = 'TMEntry , FieldEntry'
+      joi4 = "(TMEntry.TM_Date = FieldEntry.DATE) and (TMEntry.TM_Date>="
+      cur.execute(f'''select {val4} from {tab4} where {joi4}{d0}) and (TMEntry.TM_Date<={d1})''')
       rv.append(cur.fetchall()[0][0])      
 
-      column_headers =  ['TMToday', 'TMTodate', 'TMTodateLY', 'RecoveryToday', 'RecoveryTodate']
+      column_headers =  ['TMToday', 'TMToDate', 'TMToDateLY', 'RecoveryToday', 'RecoveryToDate']
       json_data = []
       json_data.append(dict(zip(column_headers, rv)))
 
@@ -63,34 +63,34 @@ def displayfactory():
       cur = mysql.connection.cursor()
       
       #DIV NAME
-      vala = "DIVTAB.DIV_NAME"
-      taba = "DIVTAB, SECTAB, FIELDENTRY"
-      joia = "(FIELDENTRY.SEC_ID=SECTAB.SEC_ID) AND (SECTAB.DIV_ID = DIVTAB.DIV_ID)"
-      joba = "FIELDENTRY.JOB_ID = 1"
-      cur.execute(f'''select {vala} from {taba} where {joia} AND {joba} and date = {d1} GROUP BY SECTAB.DIV_ID''')
+      vala = "DivTab.DIV_NAME"
+      taba = "DivTab, SecTab, FieldEntry"
+      joia = "(FieldEntry.Sec_ID=SecTab.Sec_ID) AND (SecTab.Div_Id = DivTab.Div_Id)"
+      joba = "FieldEntry.JOB_ID = 1"
+      cur.execute(f'''select {vala} from {taba} where {joia} AND {joba} and Date = {d1} GROUP BY SecTab.Div_Id''')
       rva = cur.fetchall()
 
       # GL TODAY
-      vala1 = "SUM(FIELDENTRY.GL_VAL)"
-      taba1 = "DIVTAB, SECTAB, FIELDENTRY"
-      joia1 = "(FIELDENTRY.SEC_ID=SECTAB.SEC_ID) AND (SECTAB.DIV_ID = DIVTAB.DIV_ID)"
-      joba1 = "FIELDENTRY.JOB_ID = 1"
-      cur.execute(f'''select {vala1} from {taba1} where {joia1} AND {joba1} and date = {d1} GROUP BY SECTAB.DIV_ID''')
+      vala1 = "SUM(FieldEntry.GL_Val)"
+      taba1 = "DivTab, SecTab, FieldEntry"
+      joia1 = "(FieldEntry.Sec_ID=SecTab.Sec_ID) AND (SecTab.Div_Id = DivTab.Div_Id)"
+      joba1 = "FieldEntry.JOB_ID = 1"
+      cur.execute(f'''select {vala1} from {taba1} where {joia1} AND {joba1} and Date = {d1} GROUP BY SecTab.Div_Id''')
       rva1 = cur.fetchall()
 
       #GL TODAY LAST YEA1R
-      vala2 = "SUM(FIELDENTRY.GL_VAL)"
-      taba2 = "FIELDENTRY, DIVTAB, SECTAB"
-      joia2 = "(FIELDENTRY.SEC_ID=SECTAB.SEC_ID) AND (SECTAB.DIV_ID = DIVTAB.DIV_ID)"
-      joba2 = "FIELDENTRY.JOB_ID = 1"
-      cur.execute(f'''select {vala2} from {taba2} where {joia2} AND {joba2} and date = {d11} GROUP BY SECTAB.DIV_ID''')
+      vala2 = "SUM(FieldEntry.GL_Val)"
+      taba2 = "FieldEntry, DivTab, SecTab"
+      joia2 = "(FieldEntry.Sec_ID=SecTab.Sec_ID) AND (SecTab.Div_Id = DivTab.Div_Id)"
+      joba2 = "FieldEntry.JOB_ID = 1"
+      cur.execute(f'''select {vala2} from {taba2} where {joia2} AND {joba2} and Date = {d11} GROUP BY SecTab.Div_Id''')
       rva2 = cur.fetchall()
 
       #FINE LEAF% TODAYS GL
       vala3 = "sum(FL_PER)"
-      taba3 = "FLENTRY, DIVTAB"
-      joia3 = "(FLENTRY.DIV_ID = DIVTAB.DIV_ID)"
-      cur.execute(f'''select {vala3} from {taba3} where {joia3} and date = {d1} GROUP BY DIVTAB.DIV_ID''')
+      taba3 = "FLEntry, DivTab"
+      joia3 = "(FLEntry.Div_Id = DivTab.Div_Id)"
+      cur.execute(f'''select {vala3} from {taba3} where {joia3} and Date = {d1} GROUP BY DivTab.Div_Id''')
       rva3 = cur.fetchall()
 
       w = [i[0] for i in rva]
@@ -110,25 +110,25 @@ def displayfactory():
 #10## GRADE PER FACTORY
 
       cur = mysql.connection.cursor()
-      
+
       #SUM-ALLGRADES-DATERANGE
-      cur.execute(f"SELECT SUM(SORTENTRY.SORT_KG) FROM SORTENTRY WHERE date ={d1} ")
+      cur.execute(f"SELECT SUM(SortEntry.Sort_Kg) FROM SortEntry WHERE date ={d1} ")
       rv = cur.fetchall()
 
-      #SUM-ALLGRADES-DATE
-      cur.execute(f"SELECT SUM(SORTENTRY.SORT_KG) FROM SORTENTRY WHERE date ={d1} ")
+            #SUM-ALLGRADES-DATE
+      cur.execute(f"SELECT SUM(SortEntry.Sort_Kg) FROM SortEntry WHERE date ={d1} ")
       rv3 = cur.fetchall()
 
-      #SUM-PERGRADE-DATERANGE
-      cur.execute(f"SELECT SUM(SORTENTRY.SORT_KG) FROM SORTENTRY, TEAGRADETAB WHERE SORTENTRY.TEAGRADE_ID = TEAGRADETAB.TEAGRADE_ID and date ={d1} group by TEAGRADETAB.TEAGRADE_NAME ")
+            #SUM-PERGRADE-DATERANGE
+      cur.execute(f"SELECT SUM(SortEntry.Sort_Kg) FROM SortEntry, TeaGradeTab WHERE SortEntry.TeaGrade_ID = TeaGradeTab.TeaGrade_ID and date >={d1} and date <={d2} group by TeaGradeTab.TeaGrade_Name ")
       rv1 = cur.fetchall()
 
-      #PERGRADE-DATE
-      cur.execute(f"SELECT SUM(SORTENTRY.SORT_KG) FROM SORTENTRY, TEAGRADETAB WHERE SORTENTRY.TEAGRADE_ID = TEAGRADETAB.TEAGRADE_ID and date ={d1} group by TEAGRADETAB.TEAGRADE_NAME ")
+            #PERGRADE-DATE
+      cur.execute(f"SELECT SUM(SortEntry.Sort_Kg) FROM SortEntry, TeaGradeTab WHERE SortEntry.TeaGrade_ID = TeaGradeTab.TeaGrade_ID and date ={d1} group by TeaGradeTab.TeaGrade_Name ")
       rv4 = cur.fetchall()      
 
-      #GRADE-NAME
-      cur.execute(f"SELECT TEAGRADETAB.TEAGRADE_NAME FROM SORTENTRY, TEAGRADETAB WHERE SORTENTRY.TEAGRADE_ID = TEAGRADETAB.TEAGRADE_ID and date ={d1}group by TEAGRADETAB.TEAGRADE_NAME ")
+            #GRADE-NAME
+      cur.execute(f"SELECT TeaGradeTab.TeaGrade_Name FROM SortEntry, TeaGradeTab WHERE SortEntry.TeaGrade_ID = TeaGradeTab.TeaGrade_ID and date >={d1} and date <={d2} group by TeaGradeTab.TeaGrade_Name ")
       rv2 = cur.fetchall()
 
       x = [s[0] for s in rv]
@@ -147,11 +147,11 @@ def displayfactory():
 
       zzz = zip(w,zz,z)
 
-      json_data2 = []    
+      json_data5 = []    
       column_headers = ['Grade','PercentToday','PercentTodate']
 
       for row in zzz:
-            json_data2.append(dict(zip(column_headers,row)))
+            json_data5.append(dict(zip(column_headers,row)))
       
 
       json_comp = {} 
